1906ef86d5707c957de4ad5698b8d8665ea423b0
Francisco J. Pinto Villar <francisco.pinto@parquedelrecuerdo.cl>
2025-01-10 12:47:28 -0300

    Conectando con Gateway (TCP)

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,8 @@
-import { Body, Injectable, Logger, NotFoundException, OnModuleInit, Param, ParseIntPipe } from '@nestjs/common';
+import { 
+  Injectable, 
+  Logger, 
+  NotFoundException, 
+  OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
