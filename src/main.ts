c2563d96bb414fe31f857e89ea0503a89c8af677
Francisco J. Pinto Villar <francisco.pinto@parquedelrecuerdo.cl>
2025-01-21 19:12:35 -0300

    Conectando con gateway a través de TCP

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { envs } from './config';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config';
 
 async function bootstrap() {
-
   const logger = new Logger('Main');
-  
-  // const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes( 
-  //   new ValidationPipe({ 
-  //   whitelist: true, 
-  //   forbidNonWhitelisted: true, 
-  //   }) 
-  //  );
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    {
-      transport: Transport.TCP,
-      options: {
-        port: envs.port
-      }
-    }
-  );
-  app.useGlobalPipes( 
-    new ValidationPipe({ 
-    whitelist: true, 
-    forbidNonWhitelisted: true, 
-    }) 
+  // Crea el microservicio, especificando el módulo principal (AppModule)
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: envs.port,
+    },
+  });
+
+  // Aplica los pipes globales
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
   );
 
+  // Inicia el microservicio
   await app.listen();
-  logger.log(`Microservicio corriendo en el puerto: ${envs.port}`)
+  logger.log(`Microservicio corriendo en el puerto: ${envs.port}`);
 }
+
 bootstrap();
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -9,33 +9,29 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  //@Post()
-  @MessagePattern({ cmd: 'create_product'})
+  @MessagePattern({cmd: 'create_product'})
   create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
-  //@Get()
   @MessagePattern({ cmd:'find_all_products'})
   findAll(@Payload() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
-  //@Get(':id')
   @MessagePattern({ cmd:'find_one_product'})
   findOne(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.findOne(id);
   }
 
-  //@Patch(':id')
   @MessagePattern({ cmd:'update_products'})
   update(@Payload() updateProductDto: UpdateProductDto) {
     return this.productsService.update(updateProductDto.id, updateProductDto);
   }
 
-  //@Delete(':id')
   @MessagePattern({ cmd:'delete_product'})
   remove(@Payload('id') id: string) {
     return this.productsService.remove(+id);
   }
+  
 }
