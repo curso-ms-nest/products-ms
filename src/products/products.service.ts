be732114bc089da22d3d5899a471e357f0866158
Francisco J. Pinto Villar <francisco.pinto@parquedelrecuerdo.cl>
2025-01-24 18:22:51 -0300

    Mejoras sección 5 curso Nest

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,5 @@
 import { 
+  HttpStatus,
   Injectable, 
   Logger, 
   NotFoundException, 
@@ -7,6 +8,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -26,27 +28,46 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
   async findAll(paginationDto: PaginationDto) {
     const { page, limit } = paginationDto;
+  
+    // Validar valores de paginación
+    const currentPage = page > 0 ? page : 1; // Asegurarse de que la página sea válida
+    const currentLimit = limit > 0 ? limit : 10; // Default limit si no se envía o es inválido
+  
+    // Contar el total de productos
     const totalPages = await this.product.count();
-    const lastPage = Math.ceil(totalPages/limit);
+  
+    // Calcular la última página
+    const lastPage = Math.ceil(totalPages / currentLimit);
+  
+    // Obtener los productos paginados
+    const products = await this.product.findMany({
+      skip: (currentPage - 1) * currentLimit,
+      take: currentLimit,
+    });
+  
+    // Devolver el objeto con los datos y la metadata
     return {
-      data: this.product.findMany({
-      skip: (page -1) * limit,
-      take: limit
-    }),
-    meta: {
-      total: totalPages,
-      page: page,
-      lastPage: lastPage
-    }}
+      data: products,
+      meta: {
+        total: totalPages,
+        page: currentPage,
+        lastPage: lastPage,
+      },
+    };
   }
-
-  findOne(id: number) {
-    const product =  this.product.findFirst({
-      where: {id}
+  
+  async findOne(id: number) {
+    const product =  await this.product.findFirst({
+      where: { id, available: true },
     });
 
+    console.log(product); 
+
     if(!product){
-      throw new NotFoundException(`No se encontró el producto con id: ${id}`);
+      throw new RpcException({
+        message:`No se encontró el producto con id: ${id}`,
+        code: HttpStatus.BAD_REQUEST
+      });
     }
 
     return product;
